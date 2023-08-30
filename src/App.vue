43d5c96c54c191a0dbff319bb0a443564855a141
Alan Naranjo <85073731+L-A-M-J-O@users.noreply.github.com>
2023-08-31 01:20:27 +0200

    Creacion de Arquitectura
    Install de dependencias y librerias que utilizare y organizacion de carpetas y configuracion de  plugins

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>HOLA MUNDO</h1>
+  <a class="btn btn-primary" href="#" role="button">Link</a>
+  <RouterView />
+</template>
